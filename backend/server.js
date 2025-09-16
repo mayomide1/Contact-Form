529e60ad9fa4c8a905665f6cbe2bb3fcacaa7f5b
Ayomide Mamukuyomi <ayomidemamukuyomi5@gmail.com>
2025-09-16 12:32:23 +0100

    This is a Project form where user can send a message and the message can be viewed by an admin

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mysql = require('mysql');
+const cors = require('cors');
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "login"
+})
+
+app.listen(8081, () => {
+    console.log('listening...')
+})
